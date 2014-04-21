f160729950c30456b810fb76a947cef99389693b
ekmartin <ekmartinb@gmail.com>
2014-04-21 23:38:10 +0200

    Sudo the install of pip itself too

--- python/install.sh
+++ python/install.sh
@@ -2,7 +2,7 @@ if test ! $(which pip)
 then
   echo "Downloading and installing pip:"
   curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-  python get-pip.py
+  sudo python get-pip.py
 fi
 
 echo "Installing virtualenv:"
