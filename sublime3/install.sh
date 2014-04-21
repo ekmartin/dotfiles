5d386dd9ac7f6123ea4be57cf79426b0f55f78f5
ekmartin <ekmartinb@gmail.com>
2014-04-21 02:47:20 +0200

    Update sublime stuff, change to a script you have to run manually instead

--- sublime3/install.sh
+++
@@ -1,4 +0,0 @@
-# symlink settings in
-sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-mv "$sublime_dir/User" "$sublime_dir/User.backup"
-ln -s "$DOTFILES/sublime3/User" "$sublime_dir"
