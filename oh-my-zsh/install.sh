15e7b61be7f21da54b3bd0a5a4f9f97763fa9a1a
ekmartin <ekmartinb@gmail.com>
2014-04-21 15:43:59 +0200

    Add Ryan Bates' oh-my-zsh plugin

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,20 @@
-if [ ! -d "~/.oh-my-zsh" ]
+
+omzDir=~/.oh-my-zsh
+
+if [ ! -d "$omzDir" ]
 then
+  echo "Trying to clone?"
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
+
+zsh_custom_plugins=$omzDir/custom/plugins
+rbates=$zsh_custom_plugins/rbates
+
+if [ ! -d "$rbates" ]
+then
+  echo "rbates plugin directory missing, creating it."
+  mkdir $rbates
+fi
+
+echo "Copying plugin file."
+cp $DOTFILES/oh-my-zsh/rbates.plugin $rbates/rbates.plugin.zsh
