986cac7b54a2b7c5577c4b73b9048fbcd8382514
ekmartin <ekmartinb@gmail.com>
2014-04-23 13:47:30 +0200

    Add a script that sets the iTerm tab color to green when using ssh and blue when using mosh

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,3 +3,7 @@ alias icurl="curl -I"
 
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# Set tab color
+alias ssh="color-ssh ssh"
+alias mosh="color-ssh mosh"
