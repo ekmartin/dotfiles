95b5233e451c047e0a410cd07e9dc1afa08a9e86
ekmartin <ekmartinb@gmail.com>
2014-08-08 08:14:53 +0200

    Add jump and git oh-my-zsh plugins, remove manual git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,1 @@
-# The rest of my fun git aliases
-alias gb='git branch'
-alias gc="git commit"
-alias gd='git diff'
-alias gco='git checkout'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp="git pull"
-alias gps="git push origin HEAD"
-alias gs="git status"
