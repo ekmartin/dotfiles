b84890852966d2bc348b43532ed83693772494ac
ekmartin <ekmartinb@gmail.com>
2015-03-01 18:05:47 +0100

    Add gsq git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,2 @@
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gsq="git commit --amend --no-edit"
