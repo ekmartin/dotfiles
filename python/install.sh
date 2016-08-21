49ffd87c6e4fe5c0a293e4b5bb0231302e8bf75f
Martin Ek <mail@ekmartin.com>
2016-08-22 00:33:24 +0200

    Remove manual pip install, comes with brew

--- python/install.sh
+++
@@ -1,9 +0,0 @@
-if test ! $(which pip)
-then
-  echo "Downloading and installing pip:"
-  curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-  sudo python get-pip.py
-fi
-
-echo "Installing virtualenv:"
-sudo pip install virtualenv 
